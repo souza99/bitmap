5f500a8c1a37f3d4c2eae456fe850bb41d88e3c7
João Pedro <78559316+souza99@users.noreply.github.com>
2023-09-12 21:16:48 -0300

    Update teste.cpp
    Trocado pixel da nova imagem

--- teste.cpp
+++ teste.cpp
@@ -21,26 +21,48 @@ int main()
         return 1;
     }
     
-	// set background to orange
-	image.set_all_channels(255, 150, 50);
-
 	image_drawer draw(image);
 	
-	rgb_t color;
-	
-	image.get_pixel(0, 0, color);
+	rgb_t color = image.get_pixel(2, 0);
 
-	cout << static_cast<unsigned>(color.red) << endl;
-	cout << (unsigned)color.blue << endl;
-	cout << (unsigned)color.green << endl;
-	draw.pen_width(1);
-	draw.pen_color(255, 150, 51);
-	draw.rectangle(1, 0, 1, 0);
+	unsigned cores[3];
 	
+	color = image.get_pixel(2, 0);
 
 	image.save_image("output.bmp");
 
     image.save_image("comida2.bmp");
+    
+    bitmap_image image2("output.bmp");
+    
+    color = image2.get_pixel(2, 0);
+    
+    cores[0] = static_cast<unsigned>(color.red);
+	cores[1] = static_cast<unsigned>(color.green);
+	cores[2] = static_cast<unsigned>(color.blue);
+	
+	cout << cores[0] << endl;
+	cout << cores[1] << endl;
+	cout << cores[2] << endl;
+
+    image2.set_pixel(2, 0, cores[0], cores[1], cores[2]+1);
+
+    color = image2.get_pixel(2, 0);
+    
+    int i = 0;
+	int tamanho = 40;
+    
+    //while(i <= tamanho) {}
+    
+    for (int i = 0; i <= tamanho; i++) {
+    	if(i >= image2.height()) {
+    		int tamanho_novo = tamanho - i;
+    	}
+    }
+
+    cout << static_cast<unsigned>(color.red) << endl;
+    cout << static_cast<unsigned>(color.green) << endl;
+    cout << static_cast<unsigned>(color.blue) << endl;
     
     return 0;
 }
